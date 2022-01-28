064f574004b2adab1817d011aeb24bf8f444fa38
gonjila <gonjilashvili.lasha@yahoo.com>
2022-01-28 06:39:05 +0400

    change mongoDb auth and upload on github

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 module.exports = {
     MONGODB:
-        // "mongodb+srv://gonjila:XRqnicp8FIZwNWIg@cluster0.tz9xv.mongodb.net/merng?retryWrites=true&w=majority",
         "mongodb+srv://gonjila:mBccMqCPQAH7bVb3@cluster0.tz9xv.mongodb.net/merng?retryWrites=true&w=majority",
     SECRET_KEY: "some very secret key",
 };
